e17b6f5dbe49af4f200bde58c161e0c6bac0e6ba
harshjain883 <115611849+harshjain883@users.noreply.github.com>
2023-05-04 00:25:54 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -37,88 +37,110 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 @app.on_callback_query(
     filters.regex("settings_back_helper") & ~BANNED_USERS
 )
-async def helper_private(
-    client: app, update: Union[types.Message, types.CallbackQuery]
-):
-    is_callback = isinstance(update, types.CallbackQuery)
-    if is_callback:
-        try:
-            await update.answer()
-        except:
-            pass
-        chat_id = update.message.chat.id
-        language = await get_lang(chat_id)
-        _ = get_string(language)
-        keyboard = help_pannel(_, True)
-        if update.message.photo:
-            await update.message.delete()
-            await update.message.reply_text(
-                _["help_1"], reply_markup=keyboard
-            )
-        else:
-            await update.edit_message_text(
-                _["help_1"], reply_markup=keyboard
-            )
-    else:
-        chat_id = update.chat.id
-        if await is_commanddelete_on(update.chat.id):
-            try:
-                await update.delete()
-            except:
-                pass
-        language = await get_lang(chat_id)
-        _ = get_string(language)
-        keyboard = help_pannel(_)
-        await update.reply_text(_["help_1"], reply_markup=keyboard)
-
-
-@app.on_message(
-    filters.command(HELP_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
-)
-@LanguageStart
-async def help_com_group(client, message: Message, _):
-    keyboard = private_help_panel(_)
-    await message.reply_text(
-        _["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
+async def help_pannel(_, START: Union[bool, int] = None):
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        )
+    ]
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        ),
+    ]
+    mark = second if START else first
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="ᴀᴅᴍɪɴ",
+                    callback_data="help_callback hb1",
+                ),
+                InlineKeyboardButton(
+                    text="ᴀᴜᴛʜ",
+                    callback_data="help_callback hb2",
+                ),
+                InlineKeyboardButton(
+                    text="ʙʟᴀᴄᴋʟɪsᴛ",
+                    callback_data="help_callback hb3",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="ʙʀᴏᴀᴅᴄᴀsᴛ",
+                    callback_data="help_callback hb4",
+                ),
+                InlineKeyboardButton(
+                    text="ɢʙᴀɴ",
+                    callback_data="help_callback hb12",
+                ),
+                InlineKeyboardButton(
+                    text="ʟʏʀɪᴄs",
+                    callback_data="help_callback hb5",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="ᴩɪɴɢ",
+                    callback_data="help_callback hb7",
+                ),
+                InlineKeyboardButton(
+                    text="ᴩʟᴀʏ",
+                    callback_data="help_callback hb8",
+                ),
+                InlineKeyboardButton(
+                    text="ᴩʟᴀʏʟɪsᴛ",
+                    callback_data="help_callback hb6",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="ᴠɪᴅᴇᴏᴄʜᴀᴛs",
+                    callback_data="help_callback hb10",
+                ),
+                InlineKeyboardButton(
+                    text="sᴛᴀʀᴛ",
+                    callback_data="help_callback hb11",
+                ),
+                InlineKeyboardButton(
+                    text="sᴜᴅᴏ",
+                    callback_data="help_callback hb9",
+                ),
+            ],
+            mark,
+        ]
     )
+    return upl
 
 
-@app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
-@languageCB
-async def helper_cb(client, CallbackQuery, _):
-    callback_data = CallbackQuery.data.strip()
-    cb = callback_data.split(None, 1)[1]
-    keyboard = help_back_markup(_)
-    if cb == "hb5":
-        if CallbackQuery.from_user.id not in SUDOERS:
-            return await CallbackQuery.answer(
-                "Only for Sudo Users", show_alert=True
-            )
-        else:
-            await CallbackQuery.edit_message_text(
-                helpers.HELP_5, reply_markup=keyboard
-            )
-            return await CallbackQuery.answer()
-    try:
-        await CallbackQuery.answer()
-    except:
-        pass
-    if cb == "hb1":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_1, reply_markup=keyboard
-        )
-    elif cb == "hb2":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_2, reply_markup=keyboard
-        )
-    elif cb == "hb3":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_3, reply_markup=keyboard
-        )
-    elif cb == "hb4":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_4, reply_markup=keyboard
-        )
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"], callback_data=f"close"
+                )
+            ]
+        ]
+    )
+    return upl
+
+
+def private_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="❄ ʜᴇʟᴩ ❄",
+                callback_data="settings_back_helper",
+            ),
+        ],
+    ]
+    return buttons
