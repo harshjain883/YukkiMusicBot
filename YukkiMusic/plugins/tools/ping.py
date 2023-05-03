4a8f5915045197501d9488b141d57a1a45444038
harshjain883 <115611849+harshjain883@users.noreply.github.com>
2023-05-04 00:19:53 +0530

    Update ping.py

--- YukkiMusic/plugins/tools/ping.py
+++ YukkiMusic/plugins/tools/ping.py
@@ -33,14 +33,32 @@ PING_COMMAND = get_command("PING_COMMAND")
 async def ping_com(client, message: Message, _):
     response = await message.reply_photo(
         photo=PING_IMG_URL,
-        caption=_["ping_1"],
+        caption=f"{BOT_NAME} ɪs ᴘɪɴɢɪɴɢ..."
     )
+    upt = int(time.time() - StartTime)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
     start = datetime.now()
-    pytgping = await Yukki.ping()
-    UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(
-            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
-        )
+    uptime = get_readable_time((upt))
+
+    await hmm.edit_text(
+        f"""➻ ᴩᴏɴɢ : `{resp}ᴍs`
+<b><u>{BOT_NAME} sʏsᴛᴇᴍ sᴛᴀᴛs :</u></b>
+๏ **ᴜᴩᴛɪᴍᴇ :** {uptime}
+๏ **ʀᴀᴍ :** {mem}
+๏ **ᴄᴩᴜ :** {cpu}
+๏ **ᴅɪsᴋ :** {disk}""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
+                    InlineKeyboardButton(
+                        "✨ sᴏᴜʀᴄᴇ ✨",
+                        callback_data="gib_source",
+                    ),
+                ],
+            ]
+        ),
     )
